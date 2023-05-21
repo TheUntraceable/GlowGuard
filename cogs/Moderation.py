ea088bd22db66c959f6df9ae134b5f390b764cea
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2023-05-21 13:00:37 +0100

    Add timeout commands

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,13 +1,36 @@
 from __future__ import annotations
 
 from contextlib import suppress
+from datetime import timedelta
 from typing import TYPE_CHECKING
 
-from discord import Forbidden, Interaction, Member, Permissions, File
-from discord.app_commands import Group, Range, command, describe, checks, NoPrivateMessage
+from discord import Forbidden, Interaction, Member, Permissions, File, User
+from discord.app_commands import (
+    Group,
+    Range,
+    command,
+    describe,
+    checks,
+    NoPrivateMessage,
+)
 from discord.ext.commands import Cog
 
-from .utils import WarnNotFound, generate_code, Confirm
+from .utils import (
+    WarnNotFound,
+    Confirm,
+    FailedHierarchy,
+    BotFailedHierarchy,
+    MissingGuildUserData,
+    CannotPerformActionOnBot,
+    CannotPerformActionOnSelf,
+    CannotPerformActionOnOwner,
+    CannotPerformActionOnMe,
+    InvalidDuration,
+    DurationTooLong,
+    UserNotMuted,
+    generate_code,
+    format_timedelta
+)
 
 if TYPE_CHECKING:
     from ..bot import Bot
@@ -43,17 +66,15 @@ class Moderation(Cog):
     ):
         if not interaction.guild:  # Needed to silence Ruff
             raise NoPrivateMessage
-        
-        await interaction.response.defer(
-            ephemeral=True
-        )
+
+        await interaction.response.defer(ephemeral=True)
 
         await self.bot.warns.insert_one(
             {
                 "user": user.id,
                 "reason": reason,
                 "moderator": interaction.user.id,
-                "warn_id":  generate_code(16)
+                "warn_id": generate_code(16),
             }
         )
 
@@ -61,7 +82,7 @@ class Moderation(Cog):
             await user.send(
                 f"You have been warned in {interaction.guild.name}. Reason: `{reason}`"
             )
-        
+
         await interaction.edit_original_response(
             content=f"Warned {user.mention} for `{reason}`"
         )
@@ -81,23 +102,16 @@ class Moderation(Cog):
         user: Member,
         warn_id: str,
     ):
-        if not interaction.guild: # Needed to silence Ruff
+        if not interaction.guild:  # Needed to silence Ruff
             raise NoPrivateMessage
-    
-        await interaction.response.defer(
-            ephemeral=True
-        )
 
-        deleted = await self.bot.warns.delete_one(
-            {
-                "user": user.id,
-                "warn_id": warn_id
-            }
-        )
+        await interaction.response.defer(ephemeral=True)
+
+        deleted = await self.bot.warns.delete_one({"user": user.id, "warn_id": warn_id})
 
         if deleted.deleted_count == 0:
             raise WarnNotFound
-    
+
         await interaction.edit_original_response(
             content=f"Removed warn with ID `{warn_id}` from {user.mention}"
         )
@@ -117,10 +131,8 @@ class Moderation(Cog):
     ):
         if not interaction.guild:
             raise NoPrivateMessage
-        
-        await interaction.response.defer(
-            ephemeral=True
-        )
+
+        await interaction.response.defer(ephemeral=True)
 
         warns = [warn async for warn in self.bot.warns.find({"user": user.id})]
 
@@ -143,7 +155,7 @@ class Moderation(Cog):
                     warns_file_content.encode("utf-8"),
                     f"{user.id}_warns.txt",
                 )
-            ]
+            ],
         )
 
     @warns.command(
@@ -161,7 +173,7 @@ class Moderation(Cog):
     ):
         if not interaction.guild:
             raise NoPrivateMessage
-        
+
         confirm = Confirm()
 
         await interaction.response.send_message(
@@ -172,20 +184,137 @@ class Moderation(Cog):
         await confirm.wait()
 
         if confirm.value is False:
-            await interaction.edit_original_response(
-                content="Cancelled."
-            )
+            await interaction.edit_original_response(content="Cancelled.")
 
-        await self.bot.warns.delete_many(
-            {
-                "user": user.id
-            }
-        )
+        await self.bot.warns.delete_many({"user": user.id})
 
         await interaction.edit_original_response(
             content=f"Cleared all warns for {user.mention}"
         )
 
+    @command(
+        name="mute",
+        description="Mutes a user",
+    )
+    @describe(
+        user="The user to mute",
+        reason="The reason for the mute",
+        days="The days to mute for",
+        hours="The hours to mute for",
+        minutes="The minutes to mute for",
+        seconds="The seconds to mute for",
+    )
+    @checks.has_permissions(moderate_members=True)
+    @checks.bot_has_permissions(moderate_members=True)
+    async def mute(
+        self,
+        interaction: Interaction,
+        user: Member,
+        reason: Range[str, 1, 256],
+        days: Range[int, 0, 28] = 0,
+        hours: Range[int, 0, 24] = 0,
+        minutes: Range[int, 0, 60] = 0,
+        seconds: Range[int, 0, 60] = 0,
+    ):
+        if not interaction.guild:
+            raise NoPrivateMessage
+        
+        if not self.bot.user:  # Needed to silence Ruff
+            # Can never be the case because once connected to gateway
+            # we get the data, and only then can we get interactions.
+            return  
+
+        if isinstance(interaction.user, User):
+            raise MissingGuildUserData
+
+        await interaction.response.defer(ephemeral=True)
+
+        if interaction.user.top_role <= user.top_role:
+            raise FailedHierarchy(interaction.user, user)
+        elif interaction.guild.me.top_role <= user.top_role:
+            raise BotFailedHierarchy(user)
+        elif user.id == self.bot.user.id:
+            raise CannotPerformActionOnMe
+        elif user.bot:
+            raise CannotPerformActionOnBot
+        elif user.id == interaction.user.id:
+            raise CannotPerformActionOnSelf
+        elif user.id == interaction.guild.owner_id:
+            raise CannotPerformActionOnOwner
+
+        duration = timedelta(
+            days=days,
+            hours=hours,
+            minutes=minutes,
+            seconds=seconds,
+        )
+
+        if duration.total_seconds() == 0:
+            raise InvalidDuration(duration.total_seconds())
+        
+        if duration.total_seconds() > timedelta(days=28).total_seconds():
+            raise DurationTooLong(duration.total_seconds())
+
+        await user.timeout(
+            duration,
+            reason=reason
+        )
+
+        await interaction.edit_original_response(
+            content=(
+                f"Muted {user.mention} for {format_timedelta(duration)}."
+                f"Reason: `{reason}`"
+            )
+        )
+
+    @command(
+        name="unmute",
+        description="Unmutes a user",
+    )
+    @describe(
+        user="The user to unmute",
+        reason="The reason for the unmute",
+    )
+    @checks.has_permissions(moderate_members=True)
+    @checks.bot_has_permissions(moderate_members=True)
+    async def unmute(
+        self,
+        interaction: Interaction,
+        user: Member,
+        reason: Range[str, 1, 256],
+    ):
+        if not interaction.guild:
+            raise NoPrivateMessage
+
+        if not interaction.guild:
+            raise NoPrivateMessage
+        
+        if not self.bot.user:  # Needed to silence Ruff
+            # Can never be the case because once connected to gateway
+            # we get the data, and only then can we get interactions.
+            return  
+
+        if isinstance(interaction.user, User):
+            raise MissingGuildUserData
+
+        await interaction.response.defer(ephemeral=True)
+
+        if interaction.user.top_role <= user.top_role:
+            raise FailedHierarchy(interaction.user, user)
+        elif interaction.guild.me.top_role <= user.top_role:
+            raise BotFailedHierarchy(user)
+        elif user.id == self.bot.user.id:
+            raise CannotPerformActionOnMe
+        elif user.bot:
+            raise CannotPerformActionOnBot
+        elif user.id == interaction.user.id:
+            raise CannotPerformActionOnSelf
+        elif user.id == interaction.guild.owner_id:
+            raise CannotPerformActionOnOwner
+        elif user.timed_out_until is None:
+            raise UserNotMuted(user)
+        
+        await user.timeout(None, reason=reason)
 
 async def setup(bot: Bot):
     await bot.add_cog(Moderation(bot))
